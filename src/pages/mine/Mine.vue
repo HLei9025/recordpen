c74236d6249af0e4880704e8fe9f10dea35ea2ca
herman-na <1140128129@qq.com>
2019-08-16 22:30:55 +0800

    新增个人按钮

--- src/pages/mine/Mine.vue
+++ src/pages/mine/Mine.vue
@@ -1,19 +1,71 @@
 <template>
+<div>
     <div id="mine" class="page">
         <div class="header">
-            <div></div>
+            <div class='arrow'>
+                <span class="iconfont icon-jiantou2"></span>
+            </div>
+            <div class="information">
+                <div class="wrap-img">
+                    <img src="" alt="">
+                </div>
+                <div class="wrap-name">用户名</div>
+            </div>
         </div>
 
+    <personal></personal>
     </div>
+        
+    <router-view></router-view>
 
+</div>
 </template>
 
 <script>
+import personal from './children/personal'
 export default {
+    components:{
+        [personal.name]: personal,
+
+    }
     
 }
 </script>
-
-<style>
-
+<style lang="scss" scoped>
+#mine{
+    width: 100%;
+    .header{
+        width: 100%;
+        height: 160px;
+        background:#737373;
+        .arrow{
+            position: absolute;
+            left: 11px;
+            top: 11px;
+            .icon-jiantou2{
+                font-size: 24px;
+                color: #ffffff;
+                font-weight: bold;
+            }  
+        }
+        .information{
+            position: absolute;
+            height: 63px;
+            top: 63px;
+            left: 30px;
+            .wrap-img{
+                width: 63px;
+                height: 63px;
+                float: left;
+            }
+            .wrap-name{
+                margin-left: 30px;
+                line-height: 63px;
+                font-size: 20px;
+                color: #fff;
+                float: left;
+            }
+        }
+    }
+}
 </style>
---
+++ src/pages/mine/children/personal.vue
@@ -0,0 +1,58 @@
+<template>
+<div>
+    <div class="personal" v-for="item in personalData" :key="item.id">
+        <div class="type iconfont" :class="item.icon1"></div>
+        <div class="text">{{item.name}}</div>
+        <div class="arrow iconfont" :class="item.icon2"></div>
+    </div>
+
+</div>
+</template>
+
+<script>
+export default {
+    name:'personal',
+    data(){
+        return {
+            personalData:[
+                {id:1,icon1:'icon-tongbu',name:'同步',icon2:'icon-jiantou'},
+                {id:2,icon1:'icon-yanjing',name:'外观',icon2:'icon-jiantou'},
+                {id:3,icon1:'icon-daibandianjihou',name:'待办',icon2:'icon-jiantou'},
+                {id:4,icon1:'icon-richeng',name:'日程',icon2:'icon-jiantou'},
+                {id:5,icon1:'icon-suo',name:'安全',icon2:'icon-jiantou'},
+                {id:6,icon1:'icon-shezhi',name:'高级',icon2:'icon-jiantou'},
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.personal{
+    position: absolute;
+    top: 160px;
+    left: 0;
+    width: 100%;
+    background-color: #EAEAEA;
+    padding: 0 30px 15px;
+    height: 50px;
+    border-bottom: 1px solid #737373 ;
+    border-top: 1px solid #737373 ;
+    line-height: 50px;
+    box-sizing: border-box;
+    .type{
+        font-size: 24px;
+        margin-right: 10px;
+        float: left;
+    }
+    .text{
+        font-size: 20px;
+        font-weight: bold;
+        float: left
+    }
+    .arrow{
+        float: right;
+        font-size: 18px;
+    }
+}
+</style>
